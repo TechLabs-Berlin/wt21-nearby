93a7a2a7a6fd2b4920eea772149855e2c192caf6
--list <kaiserfl@hu-berlin.de>
2022-01-30 19:14:20 +0100

    add output to input-output relation

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 
-data = pd.read_csv('data.csv', delimiter = ';')
+data = pd.read_csv('data.csv', delimiter=';')
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html', name = 'Maria')
+    return render_template('home.html', name='Maria')
 
 @app.route('/out')
 def out():
-	bezirk = request.args.get('bezirk')
-	return render_template('output.html', data=data, bezirk = bezirk)
+    bezirk = request.args.get('bezirk')
+    return render_template('output.html', data=data, bezirk=bezirk)
