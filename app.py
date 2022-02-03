ae30cb6b338f51b1575118e0974ad3693c459337
--list <kaiserfl@hu-berlin.de>
2022-02-03 16:54:05 +0100

    add Full-text search feature

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ def out():
     search = request.args.get('search')
     global df
     data = df
+    #print('data', data, 'bezirk', bezirk, 'services', services, 'search', search)
     #print('bezirk', bezirk)
     #print('DataFrame', "*"*100, data.head())
     filter_bezirk = data['bezirk'] == bezirk
     filter_services = data['services'] == services
+    filter_search = df.apply(lambda row: row.astype(str).str.contains(search).any(), axis=1)
     #print('FILTER BEZIRK', "*"*100, filter_bezirk)
     #print('FILTER SERVICES', "*"*100, filter_services)
-    data = data[(filter_bezirk) & (filter_services)]
-    print('data', data, 'bezirk', bezirk, 'services', services)
-    return render_template('output.html', data=data, bezirk=bezirk, services=services)
+    data = data[(filter_bezirk) & (filter_services) & (filter_search)]
+    return render_template('output.html', data=data, bezirk=bezirk, services=services, search=search)
